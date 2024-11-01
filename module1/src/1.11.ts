f9769761144ff842d47b0ae5eebd94285d2c9ebd
Robiul Islam <robiulislamrobi0874@gmail.com>
2024-11-01 17:01:01 +0600

    optional chaining

--- module1/src/1.11.ts
+++ module1/src/1.11.ts
@@ -3,4 +3,20 @@
 	const result1 = isAuthenticated ?? "guest"
 	const result2 = isAuthenticated? isAuthenticated: "guest"
 	console.log({result1},{result2})
+
+	type User = {
+		fullName: string;
+		address: {
+			parmanentAddress: string;
+			presentAdderes?: string;
+		}
+	}
+	const user: User = {
+		fullName: 'Rabiul Islam',
+		address: {
+			parmanentAddress: 'sherpur',
+		}
+	}
+	const presentAdderes = user.address.presentAdderes ?? 'tar present address nai re bhai'
+	console.log(presentAdderes)
 }
