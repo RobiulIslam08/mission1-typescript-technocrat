1a28828873d85ce39f86b2c509efcf07de157b4f
Robiul Islam <robiulislamrobi0874@gmail.com>
2024-11-01 18:37:31 +0600

    unknown type

--- module1/src/1.12.ts
+++ module1/src/1.12.ts
@@ -8,5 +8,20 @@
 		}
 
 	}
-	searchName(null)
+	// searchName(null)
+
+	//unknown type
+	const getSpeedInMeterPerSecond = (value:unknown) =>{
+		if(typeof value == 'number'){
+			const covertedSpeed = (value * 1000) / 3600
+			console.log(`the speed is ${covertedSpeed}`)
+		}
+		if(typeof value == 'string'){
+			const [result, unit] = value.split(' ')
+			const covertedSpeed = (parseFloat(result) * 1000) / 3600
+			console.log(covertedSpeed)
+		}
+
+	}
+	getSpeedInMeterPerSecond('1000 kmh')
 }
