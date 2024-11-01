05bc5336e63ef350cc2c8f48a754ffe7532c7537
Robiul Islam <robiulislamrobi0874@gmail.com>
2024-11-01 18:13:01 +0600

    nullable type

---
+++ module1/src/1.12.ts
@@ -0,0 +1,12 @@
+{
+	//nullable type
+	const searchName = (value:string | null) =>{
+		if(value){
+			console.log('searching')
+		}else{
+			console.log('there is nothing to search')
+		}
+
+	}
+	searchName(null)
+}
