66cfbab55e07551b2815d1821aa75cb96631bac2
Robiul Islam <robiulislamrobi0874@gmail.com>
2024-11-02 22:37:29 +0600

    type assertion

--- module2/dist/21.js
+++ module2/dist/21.js
@@ -1,4 +1,16 @@
 "use strict";
 {
-    const user = 'Rabiul';
+    const kgToGm = (value) => {
+        if (typeof value == 'string') {
+            const convertedValue = parseFloat(value) * 1000;
+            return `the converted value is ${convertedValue}`;
+        }
+        if (typeof value == 'number') {
+            return value * 1000;
+        }
+    };
+    const result1 = kgToGm(1000);
+    const result2 = kgToGm('1000');
+    console.log(result1);
+    console.log(result2);
 }
--- module2/src/21.ts
+++ module2/src/21.ts
@@ -1,5 +1,16 @@
 {
-	type User = string
-	const user: User = 'Rabiul'
-	
+	const kgToGm = (value:string | number ):number | string | undefined  => {
+		if(typeof value  == 'string'){
+			const convertedValue = parseFloat(value) * 1000;
+			
+			return `the converted value is ${convertedValue}`
+		}
+		if(typeof value == 'number') {
+			return value * 1000
+		}
+	}
+	const result1 = kgToGm(1000) as number
+	const result2 = kgToGm('1000') as string
+	console.log(result1)
+	console.log(result2)
 }
