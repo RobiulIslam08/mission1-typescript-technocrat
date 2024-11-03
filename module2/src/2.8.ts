3c4c7dedda01f244a9adb3caecdb4fa7c8f8f6c5
Robiul Islam <robiulislamrobi0874@gmail.com>
2024-11-04 00:36:02 +0600

    Asynchronous typescript

---
+++ module2/src/2.8.ts
@@ -0,0 +1,51 @@
+{
+	//Asynchronous typescript
+	type Somthing = {somthing:string}
+	const creatPromise = ():Promise<Somthing> =>{
+		return new Promise<Somthing>((resolve, reject)=>{
+			const data:Somthing = {somthing: 'something'};
+			if(data) {
+				resolve(data)
+			}else{
+				reject('failed to load data')
+			}
+		})
+	}
+	//calling create function
+	const showData = async ():Promise<Somthing> => {
+		
+			const data:Somthing = await creatPromise();
+			// console.log(data)
+			return data
+		
+	}
+
+	showData()
+
+	//data fetching example two
+	type Todo = {
+		userId:number;
+		id:number;
+		title:string;
+		completed:boolean
+	}
+	const getTodo =async():Promise<Todo> =>{
+	
+			const response = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+			const data = await response.json()
+			console.log(data)
+			return data
+		
+		
+	}
+	getTodo()
+
+
+
+
+
+
+
+
+	//
+}
